19efdfd6898d5731f722a8eea57174c8d15d84cc
bcgosselin <bcgosselin@gmail.com>
2024-01-30 23:18:10 -0600

    functioning timer and readme.md updated

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# module-4-challenge
-Full Stack Web Development Project 4
-
 //User Story
 
 AS A coding boot camp student
@@ -19,3 +16,66 @@ WHEN all questions are answered or the timer reaches 0
 THEN the game is over
 WHEN the game is over
 THEN I can save my initials and score
+
+
+# module-4-challenge
+Full Stack Web Development Project 4
+
+https://bcgosselin.github.io/module-4-challenge/
+
+## Description
+
+This project is Robert Gosselin's fourth web application challenge. This project was created to showcase his skills and aptitude using only text based instructions. This webpage's index, script, and style sheet was written by hand utilizing credited resources [Credits](#credits). The goal of this challenge was to develop and engineer a fuctioning quiz on a live website. Everything included was apart of an ongoing bootcamp to develop and hone Full Stack Web Development skills.
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+NA
+
+## Usage
+
+Feel free to view below screenshot (ctrl + click).
+
+![alt text](assets/images/screenshot.png)
+
+## Credits
+
+Utilized within var question
+    https://stackoverflow.com/questions/62594459/how-to-check-the-correct-answer-javascript-4-buttons
+
+    assistance from instructor during office hours
+
+Utilized within function startTimer
+    https://www.youtube.com/watch?v=4piMZDO5IOI&ab_channel=ReachStar
+
+
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [Robert Gosselin]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,56 @@
 var doc = document
-
-var question = [{
-	    'question' : "Commonly used data types DO Not include:",
-	    'answers' : ['strings','booleans','alerts','numbers']
-	},
-    {
-	    'question' : "The condition in an if / else statement is enclosed with _______.",
-	    'answers' : ['quotes','curly brackets','parenthesis','square brackets']
-	},
-    {
-	    'question' : "Arrays in a Javascript can be used too store",
-	    'answers' : ['numbers and strings','other arrays','booleans','all of the above']
-	},
-    {
-	    'question' : "String values must be enclosed within _______ when being assigned to variables",
-	    'answers' : ['commas','curly brackers','quotes','parenthesis']
-	},
-	{
-	    'question' : "A very useful tool used during development and debugging for printing content to the degugger is:",
-	    'answers' : ['JavaScript','terminal/bash','for loops','console.log']
-	},
-]
-
-
-function displayQuestion(){
-	var questionEl = $('<h2>')
-	questionEl.text(question[0].question);
-/*need loop for question text (appendchild?)*/
-    questions[0].answers.map((answer) => {
-        $('<button>').text(answer);
-    })
-}
-
-/*function hideText*/
-
-/*function*/
-
-/*function*/
-
-/*function*/
-
 var startBtn = doc.querySelector("#start");
 
-startBtn.addEventListener("click", displayQuestion);
+var question = [
+    {
+        'question': "Commonly used data types DO Not include:",
+        'answers': ['strings', 'booleans', 'alerts', 'numbers'],
+        'correctAnswer': 'alerts'
+    },
+    {
+        'question': "The condition in an if / else statement is enclosed with _______.",
+        'answers': ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+        'correctAnswer': 'curly brackets'
+    },
+    {
+        'question': "Arrays in a Javascript can be used to store",
+        'answers': ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+        'correctAnswer': 'all of the above'
+    },
+    {
+        'question': "String values must be enclosed within _______ when being assigned to variables",
+        'answers': ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+        'correctAnswer': 'quotes'
+    },
+    {
+        'question': "A very useful tool used during development and debugging for printing content to the debugger is:",
+        'answers': ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
+        'correctAnswer': 'console.log'
+    },
+];
+
+
+//function startTimer
+//timer start at 60 seconds paired with start button
+//if/else condition that says if q is right you get 10 points if wrong lopse 10 seconds from timer
+var timer;
+var countdown = doc.getElementById('timerDisplay')
+
+function startTimer() {
+	var sec = 60;
+	timer = setInterval(startTimer=> {
+		countdown.innerHTML = '00:' +sec;
+		sec --;
+	}, 1000)
+}
+
+//function displayQuestion
+//must hide header and display question container
+
+//function finalScore
+//access and pull if condition
+
+//function highScore
+//store finalScore function data in local storage
+
+startBtn.addEventListener("click", startTimer)
